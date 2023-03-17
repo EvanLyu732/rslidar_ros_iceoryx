5772f539f1eaeb72377a91a6c12a0ca8f0c496bc
evanlyu732 <evan.lv@uditech.com.cn>
2023-03-17 14:05:50 +0800

    update file

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/code_base/test_iceoryx_pointcloud
-# Build directory: /home/ubuntu/code_base/test_iceoryx_pointcloud/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
